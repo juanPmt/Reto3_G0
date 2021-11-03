f5bc355d9e2b49cbc202753f1f52e78644212c21
Sebastian Perez <juan.perez2.mt@correo.usa.edu.co>
2021-11-02 21:33:21 -0500

    Agregada pagina y funciones crear/visualizar admin

---
+++ P7_JSp/src/main/resources/static/js/verAdminF.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    console.log("Entre a usuarios A");
+    verAdminF();   
+});
+
+function verAdminF(){
+    //Nos trae desde el servidos la base de datos de la tabla admin
+        $.ajax({
+            url:"/api/Admin/all",
+            type: 'GET',
+            dataType: 'json',
+            success: function(respuesta){
+                console.log(respuesta);
+                mostrarRespuestaAd(respuesta);
+            },
+            error: function (xhr, status) {
+                alert('ha sucedido un problema');
+            },
+            complete: function (xhr, status) {
+                console.log(status);
+            }        
+        });    
+    }
+    
+    function mostrarRespuestaAd(items){
+        let tablaAd = `<table class="table striped" border="1">
+                      <tr>
+                        <th>Nombre</th>
+                        <th>Correo</th>                   
+                      </tr>`;                  
+        
+        for (let i=0; i < items.length; i++) {
+           
+            tablaAd +=`<tr> 
+                       <td>${items[i].name}</td>
+                       <td>${items[i].email}</td>                   
+                    </tr>`;
+        }
+        tablaAd +=`</table>`;
+    
+        $("#tablaAd").html(tablaAd);
+    }
+
